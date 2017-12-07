6342d56cada1ede81498a398bc02c9e31b3053cf
shaqeer <shaqeer@hotmail.co.uk>
2017-12-07 22:22:43 +0000

    Little Programming project
    FIZZ BUZZ FIZZBUZZ test
    Completeing the test as well as using overridding to develop multiple solutions

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveNation
+{
+    class Program
+    {
+        //develop a c# console app that accepts a number range,
+        //applies a set of rules to each number in the range
+        //and returns the results
+        static void Main(string[] args)
+        {
+        }
+    }
+}
